d5ca799da9ffd15b9700a89a413d89abcae1778d
Shawn Truesdell <satel.shawn@gmail.com>
2016-08-18 13:53:22 +0700

    Initial Commit

---
+++ Samson Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Samson Social
+//
+//  Created by Shawn Truesdell on 18/08/2016.
+//  Copyright © 2016 Satel Develops. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Samson SocialTests/Samson_SocialTests.swift
@@ -0,0 +1,36 @@
+//
+//  Samson_SocialTests.swift
+//  Samson SocialTests
+//
+//  Created by Shawn Truesdell on 18/08/2016.
+//  Copyright © 2016 Satel Develops. All rights reserved.
+//
+
+import XCTest
+@testable import Samson_Social
+
+class Samson_SocialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
