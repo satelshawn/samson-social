22eff1389502d7e6b1e7fcfa7a26ba6912dc8b16
Shawn Truesdell <shawntruesdell@Shawns-iMac.local>
2016-08-19 12:06:01 +0700

    Sign in ui custom classes complete

---
+++ Samson Social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Samson Social
+//
+//  Created by Shawn Truesdell on 19/08/2016.
+//  Copyright © 2016 Satel Develops. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120 / 255.0
